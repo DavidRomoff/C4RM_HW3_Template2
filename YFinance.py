033e965482f68b6e879d749bd84235539363e491
David Romoff <74327064+DavidRomoff@users.noreply.github.com>
2024-10-13 20:32:10 -0400

    Add files via upload

---
+++ YFinance.py
@@ -0,0 +1,7 @@
+def YahooData2returns(YahooData):
+    # Yahoo Data = raw downloaded data from Yahoo Finance using yfinance
+    # returns = % returns of 'Adj Close' as a data vector (not a data frame)
+
+    returns = np.array([ 0.01980198, -0.00970874,  0.01960784])
+    return returns
+
